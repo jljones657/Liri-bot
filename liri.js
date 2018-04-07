e19a84644fc26e2a872e8e7862ee33c65249222f
Jared Jones <jljones657@gmail.com>
2018-04-06 20:44:43 -0500

    Tweets work, but Spotify doesn't

--- liri.js
+++ liri.js
@@ -1,5 +1,91 @@
-require("dotenv").config();
+var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var Twitter = require('twitter');
+var spotify = require('spotify');
 var client = new Twitter(keys.twitter);
 var fs = require ('fs-extra');
+
+//capture user input, and inform user of what to type in.
+console.log("Type my-tweets , spotify-this-song to get started!");
+//process[2] choses action, process[3] as search parameter for spotify or movie.
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
+//process multiple words. Triggers if user types anything more than the above console logged options and first parameter.
+	for(i=4; i<process.argv.length; i++){
+	    secondCommand += '+' + process.argv[i];
+	}
+
+function theSwitch(){
+	//action statement, switch statement to declare what action to execute.
+	switch(userCommand){
+
+		case 'my-tweets':
+		fetchTweets();
+		break;
+
+		case 'spotify-this-song':
+		spotifyMe();
+		break;
+
+		
+	}
+};
+//functions/options
+function fetchTweets(){
+	console.log("Tweets Incoming!");
+	//new variable for instance of twitter, load keys from imported keys.js
+	var client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	});
+
+	//parameters for twitter function.
+	var parameters = {
+		screen_name: 'Narcissisme_pur',
+		count: 20
+	};
+
+	//call the get method on our client variable twitter instance
+	client.get('statuses/user_timeline', parameters, function(error, tweets, response){
+		if (!error) {
+	        for (i=0; i<tweets.length; i++) {
+	            var returnedData = ('Number: ' + (i+1) + '\n' + tweets[i].created_at + '\n' + tweets[i].text + '\n');
+	            console.log(returnedData);
+	            console.log("-------------------------");
+	        }
+	    };
+	});
+};//end fetchTweets;
+
+function spotifyMe(){
+	console.log("Music!");
+
+	//variable for search term, test if defined.
+
+	var searchTrack;
+	if(secondCommand === undefined){
+		searchTrack = "The Sign";
+	}else{
+		searchTrack = secondCommand;
+	}
+	//launch spotify search
+	spotify.search({type:'track', query:searchTrack}, function(err,data){
+	    if(err){
+	        console.log('Error occurred: ' + err);
+	        return;
+	    }else{
+	        //tried searching for release year! Spotify doesn't return this!
+	  		console.log("Artist: " + data.tracks.items[0].artists[0].name);
+	        console.log("Song: " + data.tracks.items[0].name);
+	        console.log("Album: " + data.tracks.items[0].album.name);
+	        console.log("Preview Here: " + data.tracks.items[0].preview_url);
+	    }
+	});
+};//end spotifyMe
+
+
+
+theSwitch();
+
